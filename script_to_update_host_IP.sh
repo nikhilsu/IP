507286088433b44c74c87e8d022538e0eca50037
nikhilsu <nikhilsu@thoughtworks.com>
2015-11-20 21:26:38 +0530

    Changed a few variable names and added the y option to sudo apt-get install command

--- script_to_update_host_IP.sh
+++ script_to_update_host_IP.sh
@@ -4,9 +4,9 @@ sudo cp script_to_update_host_IP.sh /usr/local/bin/script_to_update_host_IP.sh
 local_computer=0 
 mac_address_of_the_goserver="a0:99:9b:0d:96:1d"
 
-mac_addresses=`ifconfig|grep "ether"|awk '{print $2}'`
+local_mac_addresses=`ifconfig|grep "ether"|awk '{print $2}'`
 
-for mac_address in $mac_addresses; do
+for mac_address in local_mac_addresses; do
 	if [[ "$mac_address" == "$mac_address_of_the_goserver" ]]; then
 		local_computer=1
 	fi
@@ -18,7 +18,7 @@ if [[ "$local_computer" == "0" ]]; then
 	if [[ "$arp" == "" ]]; then
 		os=`uname -v|grep "^Darwin"`
 		if [[ "$os" == "" ]]; then
-			sudo apt-get install arp-scan
+			sudo apt-get install -y arp-scan
 		else
 			brew install arp-scan
 		fi
@@ -26,10 +26,10 @@ if [[ "$local_computer" == "0" ]]; then
 
 	ip=`sudo arp-scan --interface=en0 --localnet|grep "$mac_address_of_the_goserver" |awk '{print $1}'`
 
-	append_to_hosts_file="$ip\tgoserver.com"
+	content_to_be_appended_to_etc_hosts_file="$ip\tgoserver.com"
 
 else
-	append_to_hosts_file="127.0.0.1\tgoserver.com"
+	content_to_be_appended_to_etc_hosts_file="127.0.0.1\tgoserver.com"
 
 fi
 
@@ -38,7 +38,7 @@ line_number_in_hosts=`grep -n "goserver.com" /etc/hosts | awk -F ":" '{print $1}
 updated_contents=`cat /etc/hosts|awk -v line="$line_number_in_hosts" '{if(NR!=line) print $0}'`
 
 echo -e "$updated_contents" | cat>temp_hosts
-echo -e "$append_to_hosts_file" | cat>>temp_hosts
+echo -e "$content_to_be_appended_to_etc_hosts_file" | cat>>temp_hosts
 sudo mv temp_hosts /etc/hosts
 rm -rf temp_hosts
 # cat temp_hosts
@@ -49,7 +49,7 @@ printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <plist version=\"1.0\">
 <dict>
     <key>Label</key>
-	    <string>net.tw.changingIP</string>
+	    <string>com.tw.changingIP</string>
 
     <key>ThrottleInterval</key>
     	<integer>18000</integer>
@@ -69,6 +69,6 @@ printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 chmod +x script_to_update_host_IP.plist
 sudo chown root script_to_update_host_IP.plist
 sudo chgrp wheel script_to_update_host_IP.plist
-sudo mv script_to_update_host_IP.plist /Library/LaunchDaemons
+sudo mv script_to_update_host_IP.plist /Library/LaunchDaemons/
 
 launchctl load /Library/LaunchDaemons/script_to_update_host_IP.plist
